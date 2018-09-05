c70edcf74434681a0db7dc4178a22ae28b057eba
MD. Fozla Rabbi <fozlarabbi.cseru10@gmail.com>
2018-09-05 07:33:32 +0600

    created addition and substraction method

--- OperatorTest/src/Main/OperatorTestClass.java
+++ OperatorTest/src/Main/OperatorTestClass.java
@@ -2,4 +2,16 @@ package Main;
 
 public class OperatorTestClass {
 
+	public Object addition(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 + operand2;
+	}
+
+	public Object subtraction(int operand1, int operand2) {
+		// TODO Auto-generated method stub
+		return operand1 - operand2;
+	}
+
+	
+
 }
--- OperatorTest/src/Test/OperatorTestUnitTest.java
+++ OperatorTest/src/Test/OperatorTestUnitTest.java
@@ -4,11 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Main.OperatorTestClass;
+
 class OperatorTestUnitTest {
+	OperatorTestClass oT = new OperatorTestClass();
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertEquals(5,oT.addition(2,3),"");
+		assertEquals(5,oT.subtraction(7,2),"");
+		
 	}
 
 }
